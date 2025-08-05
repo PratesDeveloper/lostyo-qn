a438172224e90d8369e4e052b016f3531bb4f9e7
PratesDeveloper <pratesdeveloper@gmail.com>
2025-08-04 22:46:43 -0300

    Update firebase.js

--- app/api/server/firebase.js
+++ app/api/server/firebase.js
@@ -1,1 +1,18 @@
+const { initializeApp, cert, getApps } = require('firebase-admin/app');
+const { getFirestore } = require('firebase-admin/firestore');
 
+let db;
+
+if (!getApps().length) {
+  const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
+
+  initializeApp({
+    credential: cert(serviceAccount),
+  });
+  
+  db = getFirestore();
+} else {
+  db = getFirestore();
+}
+
+module.exports = db;
